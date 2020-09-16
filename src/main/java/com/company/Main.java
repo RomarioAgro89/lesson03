77af836466b3bb90d0d5463a7e521f7faeaad422
RomarioAgro89 <romio.kampotti@gmail.com>
2020-09-16 20:44:46 +0300

    CalculatorIsPower

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,7 +1,29 @@
 package com.company;
 
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        int a, b;
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите число: ");
+        a = scanner.nextInt();
+        System.out.println("Введите ещё число: ");
+        b = scanner.nextInt();
+        int result = calculator.add(a, b);
+        int result2 = calculator.minus(a, b);
+        long result3 = calculator.multiply(a, b);
+        double result4 = calculator.division(a, b);
+
+        System.out.println("Сумма ваших чисел равна: " + result);
+        System.out.println("Разность ваших чисел равна: " + result2);
+        System.out.println("Умножение ваших чисел равно: " + result3);
+        System.out.println("Деление ваших чисел равно: " + result4);
+
     }
+
 }
